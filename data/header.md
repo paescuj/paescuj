b3916902d4855a39df6b0f17dc2f1182b8157082
Pascal Jufer <pascal-jufer@bluewin.ch>
2022-08-25 12:00:01 +0200

    Add simple script to generate sections from JSON

---
+++ data/header.md
@@ -0,0 +1,11 @@
+## Welcome to my GitHub profile 👋
+
+Hi! I'm Pascal, a `/(System Engineer|DevOps|Developer)/` from Bern, Switzerland.
+
+I like to contribute to open source projects in my free time.
+
+[![Sponsor](https://img.shields.io/static/v1?label=Sponsor&message=%E2%9D%A4&logo=github)](https://github.com/sponsors/paescuj)
+
+
+[![Pascal's GitHub stats](https://github-readme-stats.vercel.app/api?username=paescuj&show_icons=true&theme=gruvbox&hide_title=true&include_all_commits=true)](https://github.com/anuraghazra/github-readme-stats)
+
---
+++ generate.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+getBadge() {
+  declare name="$1" repo="$2" type="$3"
+
+  local link="https://github.com/${repo}"
+
+  case "$type" in
+    language)
+      local alt="Top language of ${name}"
+      local url="https://img.shields.io/github/languages/top/${repo}"
+      ;;
+    license)
+      local alt="License of ${name}"
+      local url="https://img.shields.io/github/license/${repo}?label=License"
+      ;;
+    stars)
+      local alt="Stars of ${name} on GitHub"
+      local url="https://img.shields.io/github/stars/${repo}?label=Stars&logo=github"
+      ;;
+    downloads)
+      local alt="Weekly downloads of ${name} on NPM"
+      local url="https://img.shields.io/npm/dw/${name}?label=Downloads&logo=npm"
+      link="https://www.npmjs.com/package/${name}"
+      ;;
+    dependent_repos)
+      local alt="Dependent repos of ${name}"
+      local url="https://img.shields.io/librariesio/dependent-repos/npm/${name}?label=Dependent%20Repos"
+  esac
+
+  printf '[![%s](%s)](%s)' "$alt" "$url" "$link" 
+}
+
+cat data/header.md
+
+jq --compact-output 'to_entries | .[]' data/sections.json | while read section; do
+  title=$(echo "$section" | jq --raw-output '.key')
+  entries=$(echo "$section" | jq --compact-output '.value | to_entries | .[]')
+
+  printf '\n<details><summary><strong>%s</strong></summary>\n\n---\n\n' "$title"
+
+  while read entry; do
+    name=$(echo "$entry" | jq --raw-output '.key')
+    repo=$(echo "$entry" | jq --raw-output '.value.repo')
+    badges=$(echo "$entry" | jq --compact-output --raw-output '.value.badges | .[]')
+    description=$(curl --silent --header 'Accept: application/vnd.github+json' "https://api.github.com/repos/${repo}" | jq --raw-output '.description')
+
+    printf '[**%s**](%s)\n\n' "$name" "https://github.com/${repo}"
+    printf '> %s\n\n' "$description"
+
+    while read badge; do
+      printf '%s\n' "$(getBadge "$name" "$repo" "$badge")"
+    done <<< "$badges"
+
+    printf '\n---\n\n'
+  done <<< "$entries"
+
+  printf '</details>\n\n'
+done
+
