4fc0dac26152a35bba80464bf96836773a809c3b
Pascal Jufer <pascal-jufer@bluewin.ch>
2022-11-04 02:00:18 +0100

    Define refs in yaml file, add contribution count

--- data/generate-references.sh
+++ data/generate-references.sh
@@ -33,22 +33,27 @@ getBadge() {
   printf '[![%s](%s)](%s)' "$alt" "$url" "$link" 
 }
 
-jq --compact-output 'to_entries | .[]' "${SCRIPT_DIR}/references.json" | while read section; do
+yq --output-format json "${SCRIPT_DIR}/references.yml" | jq --compact-output 'to_entries | .[]' | while read section; do
   title=$(echo "$section" | jq --raw-output '.key')
-  entries=$(echo "$section" | jq --compact-output '.value | .[]')
+  entries=$(echo "$section" | jq --compact-output '.value.repos | to_entries | .[]')
 
   printf '\n<details><summary><strong>%s</strong></summary>\n<p><ul>\n' "$title"
 
   while read entry; do
     printf '<table><tr><td width="500px">\n'
-    repo=$(echo "$entry" | jq --raw-output '.repo')
+    repo=$(echo "$entry" | jq --raw-output '.key')
     owner=${repo%/*}
     name=${repo#*/}
-    npm=$(echo "$entry" | jq --raw-output --exit-status '.npm // empty' || printf "$name")
-    badges=$(echo "$entry" | jq --compact-output --raw-output '.badges | .[]')
+    values=$(echo "$entry" | jq --compact-output '.value')
+    npm=$(echo "$values" | jq --raw-output --exit-status '.npm // empty' || printf "$name")
+    badges=$(echo "$values" | jq --compact-output --raw-output '.badges | .[]')
     description=$(curl --silent --header 'Accept: application/vnd.github+json' "https://api.github.com/repos/${repo}" | jq --raw-output '.description')
 
-    printf '<p>\n\n[%s](%s) / [**%s**](%s)\n\n</p>\n\n' "$owner" "https://github.com/${owner}" "$name" "https://github.com/${repo}"
+    printf '<p>\n<a href="%s">%s</a> / <a href="%s"><b>%s</b></a>' "https://github.com/${owner}" "$owner" "https://github.com/${repo}" "$name"
+    if [[ $(echo "$section" | jq '.value.options.contribution') = 'true' ]]; then
+      printf '\n<br><sub>(contributed with %i merged pull requests)</sub>' $(curl --silent --header 'Accept: application/vnd.github+json' "https://api.github.com/search/issues?q=repo:${repo}+author:paescuj+is:merged&per_page=1" | jq --raw-output '.total_count')
+    fi
+    printf '\n</p>\n\n'
     printf '> %s\n\n' "$description"
 
     while read badge; do
